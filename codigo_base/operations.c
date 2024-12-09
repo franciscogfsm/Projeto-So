80a90e53cae18d0b138d7448308d6b91e39fd14d
Francisco Gama Franco Soares Martins <147647438+inaciobalu@users.noreply.github.com>
2024-12-09 17:44:21 +0000

    outpuit into terminal tests wrong

--- codigo_base/operations.c
+++ codigo_base/operations.c
@@ -51,64 +51,51 @@ int kvs_write(size_t num_pairs, char keys[][MAX_STRING_SIZE], char values[][MAX_
 
   return 0;
 }
-int kvs_read(size_t num_pairs, char keys[][MAX_STRING_SIZE], int fd_out) {
+
+int kvs_read(size_t num_pairs, char keys[][MAX_STRING_SIZE]) {
   if (kvs_table == NULL) {
     fprintf(stderr, "KVS state must be initialized\n");
     return 1;
   }
-
-  write(fd_out, "[", 1);
+  printf("[");
   for (size_t i = 0; i < num_pairs; i++) {
     char* result = read_pair(kvs_table, keys[i]);
-    char buffer[256];
     if (result == NULL) {
-      snprintf(buffer, sizeof(buffer), "(%s,KVSERROR)", keys[i]);
+      printf("(%s,KVSERROR)", keys[i]);
     } else {
-      snprintf(buffer, sizeof(buffer), "(%s,%s)", keys[i], result);
+      printf("(%s,%s)", keys[i], result);
     }
-    write(fd_out, buffer, strlen(buffer));
     free(result);
   }
-  write(fd_out, "]\n", 2);
+  printf("]\n");
   return 0;
 }
-
-int kvs_delete(size_t num_pairs, char keys[][MAX_STRING_SIZE], int fd_out) {
+int kvs_delete(size_t num_pairs, char keys[][MAX_STRING_SIZE]) {
   if (kvs_table == NULL) {
     fprintf(stderr, "KVS state must be initialized\n");
     return 1;
   }
   int aux = 0;
-
   for (size_t i = 0; i < num_pairs; i++) {
     if (delete_pair(kvs_table, keys[i]) != 0) {
       if (!aux) {
-        write(fd_out, "[", 1);
+        printf("[");
         aux = 1;
       }
-      char buffer[512];
-      snprintf(buffer, sizeof(buffer), "(%s,KVSMISSING)", keys[i]);
-      write(fd_out, buffer, strlen(buffer));
+      printf("(%s,KVSMISSING)", keys[i]);
     }
   }
   if (aux) {
-    write(fd_out, "]\n", 2);
+    printf("]\n");
   }
   return 0;
 }
 
-void kvs_show(int fd_out) {
+void kvs_show() {
   for (int i = 0; i < TABLE_SIZE; i++) {
     KeyNode *keyNode = kvs_table->table[i];
     while (keyNode != NULL) {
-      char buffer[256];
-      snprintf(buffer, sizeof(buffer), "(%s, %s)\n", keyNode->key, keyNode->value);
-      ssize_t bytes_written = write(fd_out, buffer, strlen(buffer));
-      if (bytes_written == -1) {
-        perror("write");
-        close(fd_out);
-        return;
-      }
+      printf("(%s, %s)\n", keyNode->key, keyNode->value);
       keyNode = keyNode->next; // Move to the next node
     }
   }
